f65eac9dc7927004621bd9f71ccec242060005e0
Manuel <h.torres.baquedano@gmail.com>
2015-09-02 22:22:40 -0300

    Instancia de cuadrado

--- TallerWebI/src/tarea1/Main.java
+++ TallerWebI/src/tarea1/Main.java
@@ -6,10 +6,12 @@ public class Main {
 		
 		try {
 			Rectangulo rectangulo = new Rectangulo(7, 2);
+			Cuadrado cuadrado = new Cuadrado();
 			
 			System.out.println("Area del rectangulo: " + rectangulo.ObtenerArea());
 			System.out.println("Perimetro del rectangulo: " + rectangulo.ObtenerPerimetro());
-			
+			System.out.println("El area del cuadrado es: "+ cuadrado.ObtenerArea());
+			System.out.println("El perimetro del cuadrado es: "+ cuadrado.ObtenerPerimtero());
 		} catch(Exception e) {
 			
 			System.out.println(e.getMessage());
